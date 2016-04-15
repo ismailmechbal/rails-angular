abe55e6237d525feaba7af602b653c32c2141338
Ismail <ismail@unik.ma>
2016-04-15 12:06:50 +0100

    Add category and note model and generate a seed using Faker

--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.destroy_all
+Category.destroy_all
+
+category = Category.create([ { name: 'Personal' }, { name: 'Family' }, { name: 'Work' }, { name: 'Noty' }, { name: 'Quotes' }])
+
+if Note.all.count == 0
+  500.times do |i|
+    Note.create!(
+      title: Faker::Hipster.sentence,
+      body: Faker::Hipster.paragraph(2),
+      category: category[rand(0..4)]
+    )
+  end
+end
