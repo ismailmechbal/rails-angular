7bacabff7d0340f143f538f29c276f09305524c8
Ismail <ismail@unik.ma>
2016-04-15 15:27:10 +0100

    Add Create, Update & Destroy to notes

--- api/app/controllers/api/v1/notes_controller.rb
+++ api/app/controllers/api/v1/notes_controller.rb
@@ -10,5 +10,38 @@ module Api::V1
       note = Note.find(params[:id])
       render json: note
     end
+
+    def create
+      note = Note.new(note_params)
+      if note.save
+        render json: note, status: 200
+      else
+        render json: { errors: note.errors }, status: 422
+      end
+    end
+
+    def update
+      note = Note.find(params[:id])
+      if note.update(note_params)
+        render json: note, status: 200
+      else
+        render json: { errors: note.errors }, status: 422
+      end
+    end
+
+    def destroy
+      note = Note.find(params[:id])
+      if note.destroy
+        render json: { status: 200 }
+      else
+        render json: { errors: note.errors }, status: 422
+      end
+    end
+
+    private
+
+    def note_params
+      params.require(:note).permit(:title, :body, :category_id)
+    end
   end
 end
